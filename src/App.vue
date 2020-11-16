02da13ad0a4ae8406a088681418adcc7eeff4b37
dedoblege <dedoblege@gmail.com>
2020-11-16 15:41:18 +0100

    adapting the code to composition api

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,21 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
-  data() {
+  setup() {
+    const uName = ref('Maximilian');
+
     return {
-      userName: 'Maximilian',
+      userName: uName,
     };
   },
+  // data() {
+  //   return {
+  //     userName: 'Maximilian',
+  //   };
+  // },
 };
 </script>
 
